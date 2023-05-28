bf73a6221760168a69f0cd0f1c828aa3091ec672
arshpreet44 <134253496+arshpreet44@users.noreply.github.com>
2023-05-28 23:38:17 +0000

    assignment2 changes

--- Assignment/TreasureChest.cs
+++ Assignment/TreasureChest.cs
@@ -3,32 +3,26 @@
     public class TreasureChest
     {
         private State _state = State.Locked;
-        private readonly Material _material;
-        private readonly LockType _lockType;
-        private readonly LootQuality _lootQuality;
+        private readonly Material _material; // The material of the treasure chest
+        private readonly LockType _lockType; // The type of lock on the treasure chest
+        private readonly LootQuality _lootQuality; // The quality of loot inside the treasure chest
 
         // Default Constructor
         public TreasureChest()
         {
-            _material = Material.Iron;
-            _lockType = LockType.Expert;
-            _lootQuality = LootQuality.Green;
+            _material = Material.Aluminum;
+            _lockType = LockType.Light;
+            _lootQuality = LootQuality.Silver;
         }
 
-        // Document these methods with XML documentation
-        public TreasureChest(State state) : this()
+        public TreasureChest(Material Alumenium , LockType Light, LootQuality Silver)
         {
-            _state = state;
+            _material = Alumenium;
+            _lockType = Light;
+            _lootQuality = Silver;
         }
 
-        public TreasureChest(Material material, LockType lockType, LootQuality lootQuality)
-        {
-            _material = material;
-            _lockType = lockType;
-            _lootQuality = lootQuality;
-        }
 
-        // This is called a getter
         public State GetState()
         {
             return _state;
@@ -36,7 +30,8 @@
 
         public State Manipulate(Action action)
         {
-            if (action == Action.Open) {
+            if (action == Action.Open)
+            {
                 Open();
             }
             return _state;
@@ -44,26 +39,47 @@
 
         public void Unlock()
         {
-            throw new NotImplementedException();
+            if (_state == State.Closed) //if it is closed, a message is prompted that it is closed.
+            {
+                _state = State.Locked; //If it is locked, it has to be unlocked
+            }
+            else if (_state == State.Open) //so if it is already open, a message is prompted.
+            {
+                Console.WriteLine("The chest is already open, so it cannot be unlocked!");
+            }
+            else if (_state == State.Locked)
+            {
+                Console.WriteLine("The chest cannot be unlocked as it is locked.");
+            }
         }
 
         public void Lock()
         {
-            throw new NotImplementedException();
+            if (_state == State.Locked)
+            {
+                _state = State.Closed; //if it is closed , it will be locked
+            }
+            else if (_state == State.Locked) //if it already locked, a message is prompted
+            {
+                System.Console.WriteLine("The chest is already Locked!");
+            }
+            else if (_state == State.Open) //if it is already opened, message is prompted
+            {
+                System.Console.WriteLine("The chest cannot be locked because it is open.");
+            }
         }
 
         public void Open()
         {
-            // We should check if the chest is closed
-            if (_state == State.Closed)
+            if (_state == State.Open) // if it already opened , a message is prompted
             {
-                _state = State.Open;
+                _state = State.Closed; // if it is closed , it will be opened
             }
-            else if (_state == State.Open)
+            else if (_state == State.Open) 
             {
-                Console.WriteLine("The chest is already open!");
+                Console.WriteLine("The chest is already opened!");
             }
-            else if (_state == State.Locked)
+            else if (_state == State.Locked) //if it is locked, a message is prompted
             {
                 Console.WriteLine("The chest cannot be opened because it is locked.");
             }
@@ -71,7 +87,18 @@
 
         public void Close()
         {
-            throw new NotImplementedException();
+            if (_state == State.Open) //if it is opened, it will be closed
+            {
+                _state = State.Closed; // ig it is already closed, a message is prompted
+            }
+            else if (_state == State.Closed)
+            {
+                Console.WriteLine("The chest is already closed!");
+            }
+            else if (_state == State.Locked) // if it is already locked, a message is prompted
+            {
+                Console.WriteLine("The chest cannot be closed because it is locked.");
+            }
         }
 
         public override string ToString()
@@ -86,8 +113,22 @@
 
         public enum State { Open, Closed, Locked };
         public enum Action { Open, Close, Lock, Unlock };
-        public enum Material { Oak, RichMahogany, Iron };
-        public enum LockType { Novice, Intermediate, Expert };
-        public enum LootQuality { Grey, Green, Purple };
+        public enum Material{Oak, RichMahogany, Iron, Aluminum};
+        public enum LockType{Novice, Intermediate, Expert, Light}
+        public enum LootQuality{Grey, Green, Purple, Silver}
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            TreasureChest chest = new TreasureChest(); // create a new instance
+
+            Console.WriteLine(chest.ToString()); //print the initial state
+            chest.Manipulate(TreasureChest.Action.Open); //manupulate it by opening it
+            Console.WriteLine(chest.ToString()); //print the status of chest
+
+
+        }
     }
 }
